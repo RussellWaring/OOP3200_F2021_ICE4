ab915d4ac95fcf4a268f3e5ffffc62315957a26a
RussellWaring <russell.waring@dcmail.ca>
2021-10-08 01:42:35 -0400

    Added Transform class

---
+++ OOP3200_F2021_ICE4/Transform.h
@@ -0,0 +1,25 @@
+/*
+ * @author	Russell Waring
+ * @date	2021-09-27
+ * @desc	Created following along in week 4, class a.
+ *
+ *
+ */
+
+#pragma once
+#ifndef __TRANSFORM__
+#define __TRANSFORM__
+
+#include "Vector2D.h"
+
+struct Transform
+{
+public:
+	Vector2D position;
+	Vector2D rotation;
+	Vector2D scale;
+
+	std::string ToString() const;
+};
+
+#endif /* defined (__TRANSFORM__) */
